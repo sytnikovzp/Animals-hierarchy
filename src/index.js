2b48a52e051e02f0405d8d55666327f7785e1e46
alex <sytnikov.zp@gmail.com>
2024-02-27 11:35:28 +0200

    Fix Create 2 objects of each class

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ class Animals {
     return obj instanceof this;
   }
 
-  constructor(type, age, name, weight, limbAmount, maxAge, maxWeight) {
-    this.type = type;
+  constructor(age, name, weight, limbAmount, maxAge, maxWeight) {
     this.age = age;
     this.name = name;
     this.weight = weight;
@@ -17,15 +16,15 @@ class Animals {
   }
 
   move() {
-    console.log(`${this.constructor.name} just moving anywhere like animals`);
+    console.log(`${this.name} just moving anywhere like ${this.constructor.name}`);
   }
 
   say() {
-    console.log(`${this.constructor.name} can just talk like animals`);
+    console.log(`${this.name} can just talk like ${this.constructor.name}`);
   }
 
   eat() {
-    console.log(`${this.constructor.name} can just eat like animals`);
+    console.log(`${this.name} can just eat like ${this.constructor.name}`);
   }
 }
 
@@ -37,13 +36,8 @@ class Mammals extends Animals {
     return obj instanceof this;
   }
 
-  constructor(type, age, name, weight, limbAmount, maxAge, maxWeight) {
-    super(type);
-    this.type = type;
-    this.age = age;
-    this.name = name;
-    this.weight = weight;
-    this.limbAmount = limbAmount;
+  constructor(age, name, weight, limbAmount, maxAge, maxWeight) {
+    super(age, name, weight, limbAmount, maxAge, maxWeight);
     this.maxAge = maxAge;
     this.maxWeight = maxWeight;
   }
@@ -57,13 +51,8 @@ class Birds extends Animals {
     return obj instanceof this;
   }
 
-  constructor(type, age, name, weight, limbAmount, maxAge, maxWeight) {
-    super(type);
-    this.type = type;
-    this.age = age;
-    this.name = name;
-    this.weight = weight;
-    this.limbAmount = limbAmount;
+  constructor(age, name, weight, limbAmount, maxAge, maxWeight) {
+    super(age, name, weight, limbAmount, maxAge, maxWeight);
     this.maxAge = maxAge;
     this.maxWeight = maxWeight;
   }
@@ -89,13 +78,8 @@ class Fish extends Animals {
     return obj instanceof this;
   }
 
-  constructor(type, age, name, weight, limbAmount, maxAge, maxWeight) {
-    super(type);
-    this.type = type;
-    this.age = age;
-    this.name = name;
-    this.weight = weight;
-    this.limbAmount = limbAmount;
+  constructor(age, name, weight, limbAmount, maxAge, maxWeight) {
+    super(age, name, weight, limbAmount, maxAge, maxWeight);
     this.maxAge = maxAge;
     this.maxWeight = maxWeight;
   }
@@ -121,13 +105,8 @@ class Predators extends Mammals {
     return obj instanceof this;
   }
 
-  constructor(type, age, name, weight, limbAmount, maxAge, maxWeight) {
-    super(type);
-    this.type = type;
-    this.age = age;
-    this.name = name;
-    this.weight = weight;
-    this.limbAmount = limbAmount;
+  constructor(age, name, weight, limbAmount, maxAge, maxWeight) {
+    super(age, name, weight, limbAmount, maxAge, maxWeight);
     this.maxAge = maxAge;
     this.maxWeight = maxWeight;
   }
@@ -141,13 +120,8 @@ class Whales extends Mammals {
     return obj instanceof this;
   }
 
-  constructor(type, age, name, weight, limbAmount, maxAge, maxWeight) {
-    super(type);
-    this.type = type;
-    this.age = age;
-    this.name = name;
-    this.weight = weight;
-    this.limbAmount = limbAmount;
+  constructor(age, name, weight, limbAmount, maxAge, maxWeight) {
+    super(age, name, weight, limbAmount, maxAge, maxWeight);
     this.maxAge = maxAge;
     this.maxWeight = maxWeight;
   }
@@ -161,13 +135,8 @@ class Primates extends Mammals {
     return obj instanceof this;
   }
 
-  constructor(type, age, name, weight, limbAmount, maxAge, maxWeight) {
-    super(type);
-    this.type = type;
-    this.age = age;
-    this.name = name;
-    this.weight = weight;
-    this.limbAmount = limbAmount;
+  constructor(age, name, weight, limbAmount, maxAge, maxWeight) {
+    super(age, name, weight, limbAmount, maxAge, maxWeight);
     this.maxAge = maxAge;
     this.maxWeight = maxWeight;
   }
@@ -178,9 +147,10 @@ class Dog extends Predators {
     return obj instanceof this;
   }
 
-  constructor(type) {
-    super(type);
-    this.type = type;
+  constructor(age, name, weight, limbAmount, maxAge, maxWeight) {
+    super(age, name, weight, limbAmount, maxAge, maxWeight);
+    this.maxAge = maxAge;
+    this.maxWeight = maxWeight;
   }
 
   move() {
@@ -204,13 +174,8 @@ class Dolphin extends Whales {
     return obj instanceof this;
   }
 
-  constructor(type, age, name, weight, limbAmount, maxAge, maxWeight) {
-    super(type);
-    this.type = type;
-    this.age = age;
-    this.name = name;
-    this.weight = weight;
-    this.limbAmount = limbAmount;
+  constructor(age, name, weight, limbAmount, maxAge, maxWeight) {
+    super(age, name, weight, limbAmount, maxAge, maxWeight);
     this.maxAge = maxAge;
     this.maxWeight = maxWeight;
   }
@@ -236,13 +201,8 @@ class Human extends Primates {
     return obj instanceof this;
   }
 
-  constructor(type, age, name, weight, limbAmount, maxAge, maxWeight) {
-    super(type);
-    this.type = type;
-    this.age = age;
-    this.name = name;
-    this.weight = weight;
-    this.limbAmount = limbAmount;
+  constructor(age, name, weight, limbAmount, maxAge, maxWeight) {
+    super(age, name, weight, limbAmount, maxAge, maxWeight);
     this.maxAge = maxAge;
     this.maxWeight = maxWeight;
   }
@@ -262,40 +222,46 @@ class Human extends Primates {
 
 // ----------------- Create 2 objects of each class ----------------------
 
-const animal1 = new Animals('random animal 1',10,'randAnimalName 1',30,4,20,50);
-const animal2 = new Animals('random animal 2',15,'randAnimalName 2',25,2,30,60);
+const animal1 = new Animals(10,'randAnimalName 1',30,4,20,50);
+const animal2 = new Animals(15,'randAnimalName 2',25,2,30,60);
 
-// constructor(type, age, name, weight, limbAmount, maxAge, maxWeight)
+const mammal1 = new Mammals(5, 'Fox', 12, 4, 25, 10);
+const mammal2 = new Mammals(7, 'Wolf', 17, 4, 10, 110);
 
-const mammal1 = new Mammals('fox', 5, 'FoxName', 12, 4, 25, 10);
-const mammal2 = new Mammals('wolf', 7, 'WolfName', 17, 4, 10, 110);
+const bird1 = new Birds(2, 'Pigeon', 0.5, 2, 15, 1);
+const bird2 = new Birds(1, 'Parrot', 0.4, 2, 10, 0.8);
 
-const bird1 = new Birds('pigeon', 2, 'PigeonName', 0.5, 2, 15, 1);
-const bird2 = new Birds('parrot', 1, 'ParrotName', 0.4, 2, 10, 0.8);
+const fish1 = new Fish(3,'Carp', 2.2, 'none', 5, 7);
+const fish2 = new Fish(4,'Herring', 0.8, 'none', 8, 2);
 
-const fish1 = new Fish('carp', 3,'CarpName', 2.2, 'none', 5, 7);
-const fish2 = new Fish('herring', 4,'HerringName', 0.8, 'none', 8, 2);
+const predator1 = new Predators(10, 'Lion', 50, 4, 20, 150);
+const predator2 = new Predators(5, 'Tigen', 40, 4, 25, 180);
 
-const predator1 = new Predators('lion', 10, 'LionName', 50, 4, 20, 150);
-const predator2 = new Predators('tiger', 5, 'TigenName', 40, 4, 25, 180);
+const whale1 = new Whales(8, 'Beluga', 70, 'none', 10, 100);
+const whale2 = new Whales(4, 'Blue Whale', 250, 'none', 25, 300);
 
-const whale1 = new Whales('beluga', 8, 'BelugaName', 70, 'none', 10, 100);
-const whale2 = new Whales('blue whale', 4, 'BlueWhaleName', 250, 'none', 25, 300);
+const primate1 = new Primates(6, 'Chimpanzee', 12, 4, 50, 80);
+const primate2 = new Primates(9, 'Orangutang', 90, 4, 70, 160);
 
-// const primate1 = new Primates('chimpanzee', 6, 'ChimpanzeeName');
-const primate2 = new Primates();
+const dog1 = new Dog(4, 'Terrier', 3, 4, 18, 8);
+const dog2 = new Dog(5, 'Labrador', 15, 4, 25, 30);
 
-const dog1 = new Dog();
-const dog2 = new Dog();
+const dolphin1 = new Dolphin(20, 'Striped dolphin', 70, 'none', 60, 140);
+const dolphin2 = new Dolphin(25, 'Spotted dolphin', 75, 'none', 65, 155);
 
-const dolphin1 = new Dolphin();
-const dolphin2 = new Dolphin();
-
-const human1 = new Human();
-const human2 = new Human();
+const human1 = new Human(30, 'Alex', 85, 4, 110, 150);
+const human2 = new Human(31, 'Nadia', 54, 4, 110, 120);
 
 // ----------------------------- Testings -----------------------------------
 
 console.log(animal1);
+animal1.name;
+animal1.age;
+animal1.weight
+animal1.eat;
+animal1.limbAmount;
+animal1.maxAge;
+animal1.move;
+animal1.say;
 
 console.log('--------------------------');
